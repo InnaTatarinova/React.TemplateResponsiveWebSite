80fbed39b87a9e6c4c173ea3c267a460fcf6e09f
InnaTatarinova <inna.v.tatarinova@gmail.com>
2023-03-01 14:00:57 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import "./App.css";
+import { Home } from "./pages/Home";
+import Navbar from "./components/Navbar/Navbar";
+import { Products } from "./pages/Products";
+import { Services } from "./pages/Services";
+import { SignUp } from "./pages/SignUp";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path="/" exact element={<Home/>} />
+          <Route path ="/products" element={<Products/>} />
+          <Route path ="/services" element={<Services/>} />
+          <Route path ="/sign-up" element={<SignUp/>} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
